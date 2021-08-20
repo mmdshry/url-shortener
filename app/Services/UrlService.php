be8ee37eedea9659528e59e5652a6be875d4dba2
Mohammad Shahriari Fard <unshields@gmail.com>
2021-08-20 16:51:25 +0430

    Enabled Facade and Eloquent Add index.blade.php view Add UrlService.php service Edit route web.php

---
+++ app/Services/UrlService.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Services;
+
+/**
+ *This service will handle the functions related to the URL
+ */
+class UrlService
+{
+
+    /**
+     * @param string $url
+     *
+     * @return bool
+     */
+    final public function validateUrl(string $url): bool
+    {
+        return (bool)filter_var($url, FILTER_VALIDATE_URL);
+    }
+
+}
